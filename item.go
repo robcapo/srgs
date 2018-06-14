e9d9b08b1038327bb01d33527b90fa23de2e3517
Rob Capo <Rob.Capo@ipsoft.com>
2018-06-14 16:00:08 -0400

    Eliminated currentRepeat

--- item.go
+++ item.go
@@ -8,8 +8,7 @@ type Item struct {
 	str  string
 	mode MatchMode
 
-	currentRepeat int
-	nextInd       int
+	nextInd int
 }
 
 func (it *Item) Copy(refs RuleRefs) Expansion {
@@ -19,13 +18,12 @@ func (it *Item) Copy(refs RuleRefs) Expansion {
 	}
 
 	return &Item{
-		children:      children,
-		repeatMin:     it.repeatMin,
-		repeatMax:     it.repeatMax,
-		str:           it.str,
-		mode:          it.mode,
-		currentRepeat: it.currentRepeat,
-		nextInd:       it.nextInd,
+		children:  children,
+		repeatMin: it.repeatMin,
+		repeatMax: it.repeatMax,
+		str:       it.str,
+		mode:      it.mode,
+		nextInd:   it.nextInd,
 	}
 }
 
@@ -46,7 +44,6 @@ func (it *Item) Match(str string, mode MatchMode) {
 	it.str = str
 	it.mode = mode
 
-	it.currentRepeat = it.repeatMin
 	it.nextInd = 0
 
 	it.children[0].Match(str, mode)
@@ -86,29 +83,34 @@ func (it *Item) Next() (string, error) {
 		}
 	}
 
-	if it.currentRepeat > 0 {
-		str, err = it.children[it.currentRepeat-1].Next()
-	}
+	str, err = it.children[it.nextInd].Next()
 
 	if err != nil {
-		it.currentRepeat--
-		it.nextInd = it.currentRepeat
+		it.nextInd--
 
-		if it.currentRepeat < it.repeatMin {
-			it.currentRepeat = it.repeatMin
+		str2, err2 := it.Next()
+
+		if err2 == nil {
+			return str2, err2
 		}
+
+		if err == PrefixOnly {
+			return str, err
+		}
+
+		return str2, err2
 	}
 
-	if it.currentRepeat < len(it.children) {
-		it.children[it.currentRepeat].Match(str, it.mode)
-		it.currentRepeat++
+	if it.nextInd+1 < len(it.children) {
+		it.nextInd++
+		it.children[it.nextInd].Match(str, it.mode)
 	}
 
 	return str, err
 }
 
 func (it *Item) Scan(processor Processor) {
-	for i := 0; i < it.currentRepeat; i++ {
+	for i := 0; i < it.nextInd+1; i++ {
 		it.children[i].Scan(processor)
 	}
 }
